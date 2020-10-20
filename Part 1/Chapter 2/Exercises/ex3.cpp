e99a071daceeda45cb8c2f2f95c97671183f9a7b
Jan Kowalski <yakov85@o2.pl>
2020-10-20 14:53:52 +0200

    Slider

--- Part 1/Chapter 2/Exercises/ex3.cpp
+++ Part 1/Chapter 2/Exercises/ex3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <cmath>
 #include "../Programs/arithmetic_functions.h"
 
 #include <SFML/Graphics.hpp>
@@ -22,18 +23,48 @@ int main(){
 
   // Graphical Slider
 
+  const int P = 20; const int LENGTH = 900; const int WIDTH = 1e2;
+  const int SCALE = 293; const int SHIFT = -15; const int CHAR_SIZE = 16;
+  const auto FONT_COLOR = sf::Color::Black; const float SPEED = 70;
+
+  sf::Font font; font.loadFromFile("Quesha-gndR.ttf");
+
+  std::array<std::unique_ptr<sf::Text>,P> topLabs;
+  std::array<std::unique_ptr<sf::Text>,P> botLabs;
+
+  // Create Labels
+  for(int i = 0; i<P; i++){
+
+    const std::string S = std::to_string(i+1);
+    const auto LOGV = log(i+1);
+
+    // Top Slider
+    topLabs.at(i) = std::make_unique<sf::Text>(S,font);
+    topLabs.at(i)->setCharacterSize(CHAR_SIZE);
+    topLabs.at(i)->setFillColor(FONT_COLOR);
+    topLabs.at(i)->setPosition(LOGV*SCALE-SHIFT,97);
+
+    // Bottom Slider
+    botLabs.at(i) = std::make_unique<sf::Text>(S,font);
+    botLabs.at(i)->setCharacterSize(CHAR_SIZE);
+    botLabs.at(i)->setFillColor(FONT_COLOR);
+    botLabs.at(i)->setPosition(LOGV*SCALE-SHIFT,125);
+
+  }
+
+  // Create window
   sf::RenderWindow wnd(sf::VideoMode(1024, 768), "Slider", sf::Style::Close);
   wnd.setFramerateLimit(30);
 
-  const float SPEED = 100;
   bool movingRight = false; bool movingLeft = false;
 
-  sf::RectangleShape shape1(sf::Vector2f(500, 100));
+  // Create top slider
+  sf::RectangleShape shape1(sf::Vector2f(LENGTH, WIDTH));
   shape1.setFillColor(sf::Color::Green);
   shape1.setPosition(10, 20);
 
-
-  sf::RectangleShape shape2(sf::Vector2f(500, 100));
+  // Create bottom slider
+  sf::RectangleShape shape2(sf::Vector2f(LENGTH, WIDTH));
   shape2.setFillColor(sf::Color::Red);
   shape2.setPosition(10, 125);
 
@@ -51,15 +82,19 @@ int main(){
 
 			timeSinceLastUpdate -= TimePerFrame;
 
-			// PROCESS INPUT
+			// Process input
       sf::Event event;
     	while(wnd.pollEvent(event)){
+
         switch (event.type){
+
     			case sf::Event::KeyPressed:
     				if(event.key.code == sf::Keyboard::Left){
               movingLeft = true;
             }else if(event.key.code == sf::Keyboard::Right){
               movingRight = true;
+            }else if(event.key.code == sf::Keyboard::Escape){
+              wnd.close();
             }
     				break;
 
@@ -78,16 +113,36 @@ int main(){
 
     	}
 
-      // UPDATE
-      if(movingRight){ shape2.move(SPEED * dt.asSeconds(), 0.f); }
-      if(movingLeft){ shape2.move(-SPEED * dt.asSeconds(), 0.f); }
+      // Update sliders and labels
+      if(movingRight){
+        shape2.move(SPEED * dt.asSeconds(), 0.f);
+        for(auto i=botLabs.begin();i!=botLabs.end();i++){
+          (*i)->move(SPEED * dt.asSeconds(), 0.f);
+        }
+      }
+
+      if(movingLeft){
+        shape2.move(-SPEED * dt.asSeconds(), 0.f);
+        for(auto i=botLabs.begin();i!=botLabs.end();i++){
+          (*i)->move(-SPEED * dt.asSeconds(), 0.f);
+        }
+      }
 
 		}
 
-    // RENDER
+
     wnd.clear();
+
+    // Render sliders
   	wnd.draw(shape1);
     wnd.draw(shape2);
+
+    // Render labels
+    for(auto i = 0;i<P;i++){
+      wnd.draw(*topLabs.at(i));
+      wnd.draw(*botLabs.at(i));
+    }
+
     wnd.display();
 
 	}
