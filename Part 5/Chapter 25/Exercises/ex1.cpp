cda09b536ac0d5afb927a3e754bc5f70d4666df3
Jan Kowalski <yakov85@o2.pl>
2021-08-10 21:35:08 +0200

    Heads or Tails AI

--- Part 5/Chapter 25/Exercises/ex1.cpp
+++ Part 5/Chapter 25/Exercises/ex1.cpp
@@ -1,7 +1,82 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 
 using std::cout;
+using std::cin;
+
+char checkPattern(const std::vector<char> & vec);
 
 int main(){
-  cout << "Heads or Tails\n";
+
+  srand(time(0));
+
+  const int numGames = 30;
+  char input;
+  char computerChoice;
+  int points = 0;
+
+  std::vector<char> vec;
+
+  for(int i=0;i<numGames;i++){
+
+    computerChoice =
+      0 == i ?
+        0 == rand() % 2 ? 'h' : 't' :
+        i < 3 ?
+          't' == vec.back() ? 'h' : 't'  :
+          checkPattern(vec);
+
+    do {
+      cout << "Round " << i+1 << '\n';
+      cout << "Heads(h) or Tails(t): ";
+      cin >> input; while(cin.get()!='\n'){ continue; }
+    } while(!('h'==input||'t'==input));
+
+    vec.push_back(input);
+
+    if(computerChoice!=input){
+      cout << "Win\n";
+      points++;
+    } else {
+      cout << "Lose\n";
+    }
+
+  }
+
+  cout << "Score: " << points << '/' << numGames << '\n';
+
+}
+
+char checkPattern(const std::vector<char> & vec){
+
+  const int len = vec.size();
+  const int blIndex = len - 2;
+
+  int heads, tails;
+  heads = tails = 0;
+
+  // Look for pattern in past moves
+  for(int i=0;i<blIndex;i++){
+
+    // Two moves match
+    if(vec[i]==vec[blIndex] && vec[i+1]==vec.back()){
+
+      char thirdMove = vec[i+2];
+
+      // Increase chance of 'h' or 't'
+      if('h'==thirdMove){
+        heads++;
+      } else if('t'==thirdMove){
+        tails++;
+      }
+
+    }
+
+  }
+
+  // Return symbol with higher chance
+  return heads > tails ? 'h' : 't';
+
 }
--- Part 5/Chapter 25/Exercises/ex2.cpp
+++ Part 5/Chapter 25/Exercises/ex2.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <array>
 
+using std::array;
 using std::cout;
 
+typedef array<float,2> vec2d;
+
+class Boid{
+public:
+  vec2d position;
+  vec2d velocity;
+  void showPosition() const;
+  void showVelocity() const;
+  Boid();
+};
+
+const int numBoids = 5;
+const int posRange = 201;
+const int velRange = 3;
+
 int main(){
+
+  srand(time(0));
+
+  array<Boid,numBoids> boids;
+
+  for(const Boid & b : boids){
+    b.showPosition();
+    b.showVelocity();
+  }
+
   cout << "Flocking Boids\n";
+
+}
+
+void Boid::showPosition() const{
+  cout << position[0] << ", " << position[1] << '\n';
+}
+
+void Boid::showVelocity() const{
+  cout << velocity[0] << ", " << velocity[1] <<  '\n';
+}
+
+Boid::Boid(){
+  for(int j=0;j<2;j++){
+    position[j] = rand() % posRange;
+    velocity[j] = rand() % velRange;
+  }
 }
